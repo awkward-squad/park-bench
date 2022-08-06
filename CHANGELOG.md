a692ac69d4ad99e12ce866b8494ddf857bb1da67
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-08-06 10:50:03 -0400

    fix small bug in whnf, bump to 0.1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.0.1] - 22-08-06
+
+- Fix whnf to depend on IO state token in all branches. (See GHC #21948 or criterion #264)
+
 ## [0.1.0] - 21-12-17
 
 - Initial release.
--- src/ParkBench/BenchmarkInternal.hs
+++ src/ParkBench/BenchmarkInternal.hs
@@ -6,6 +6,7 @@ module ParkBench.BenchmarkInternal
   )
 where
 
+import Control.Exception (evaluate)
 import Data.Word (Word64)
 import Prelude
 
@@ -16,8 +17,12 @@ whnf f x =
     go :: Word64 -> IO ()
     go = \case
       0 -> pure ()
-      -- `f x` won't be let-floated out with -fno-full-laziness
-      n -> f x `seq` go (n -1)
+      n -> do
+        -- `f x` won't be let-floated out with -fno-full-laziness
+        -- use evaluate rather than seq so both branches depend on the state token
+        --   https://gitlab.haskell.org/ghc/ghc/-/issues/21948
+        _ <- evaluate (f x)
+        go (n - 1)
 -- prevent `whnf f x` from inlining to prevent `f x` from getting let-floated
 {-# NOINLINE whnf #-}
 
@@ -30,5 +35,5 @@ whnfIO io =
       0 -> pure ()
       n -> do
         result <- io
-        result `seq` go (n -1)
+        result `seq` go (n - 1)
 {-# NOINLINE whnfIO #-}
