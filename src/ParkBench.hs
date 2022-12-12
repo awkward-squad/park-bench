fccc4683081ef5daa3e8484fa932c89bb80c77cc
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-12-11 20:59:18 -0500

    implement simple Terminal abstraction

--- src/ParkBench.hs
+++ src/ParkBench.hs
@@ -7,14 +7,11 @@ module ParkBench
 where
 
 import Control.Concurrent (threadDelay)
-import qualified Data.ByteString as ByteString
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Text as Text (pack)
-import qualified Data.Text.Encoding as Text (encodeUtf8)
 import ParkBench.Benchable (Benchable)
 import qualified ParkBench.Benchable as Benchable
-import qualified ParkBench.Builder as Builder
 import qualified ParkBench.Driver as Driver
 import qualified ParkBench.Measure as Measure
 import ParkBench.Named (Named (Named))
@@ -23,8 +20,7 @@ import ParkBench.Prelude
 import ParkBench.Pretty (renderTable)
 import ParkBench.Render (estimatesToTable)
 import ParkBench.RtsStats (RtsStats)
-import qualified ParkBench.Statistics as Statistics
-import ParkBench.Terminal (clearFromCursor, cursorUp, withTerminal)
+import ParkBench.Terminal (renderToTerminal, withTerminal)
 
 -- | A single benchmark.
 newtype Benchmark
@@ -43,41 +39,28 @@ benchmark benchmarks =
 
 benchmarkOne :: Named (Benchable ()) -> IO void
 benchmarkOne benchable =
-  withTerminal do
-    let loop :: Driver.Pull1 RtsStats -> Int -> IO void
-        loop pull0 newlines0 = do
+  withTerminal \terminal -> do
+    let loop :: Driver.Pull1 RtsStats -> IO void
+        loop pull0 = do
           (estimate, pull1) <- Driver.pull1 pull0
-          newlines1 <- renderSummaries ((benchable $> estimate) :| []) newlines0
-          loop pull1 newlines1
-    ByteString.putStr (ByteString.singleton newline)
-    loop (Driver.benchmark1 (Benchable.mapIO Measure.measure (Named.thing benchable))) 0
+          renderToTerminal terminal (renderTable (estimatesToTable ((benchable $> estimate) :| [])))
+          loop pull1
+    loop (Driver.benchmark1 (Benchable.mapIO Measure.measure (Named.thing benchable)))
 
 benchmarkMany :: NonEmpty (Named (Benchable ())) -> IO void
 benchmarkMany benchables =
-  withTerminal do
+  withTerminal \terminal -> do
     summaries0 <-
       (traverse . traverse)
         (\benchable -> Driver.benchmark (Benchable.mapIO Measure.measure benchable))
         benchables
-    let loop :: Driver.Pulls RtsStats -> Int -> IO void
-        loop pulls0 newlines0 = do
+    let loop :: Driver.Pulls RtsStats -> IO void
+        loop pulls0 = do
           summaries <- traverse (traverse fst) summaries0
-          newlines1 <- renderSummaries summaries newlines0
+          renderToTerminal terminal (renderTable (estimatesToTable summaries))
           pulls1 <- Driver.pull pulls0
-          loop pulls1 newlines1
-    ByteString.putStr (ByteString.singleton newline)
-    loop (Driver.pulls (snd . Named.thing <$> summaries0)) 0
-
-renderSummaries :: NonEmpty (Named (Statistics.Estimate RtsStats)) -> Int -> IO Int
-renderSummaries summaries newlines0 = do
-  ByteString.putStr bytes
-  pure (ByteString.count newline bytes)
-  where
-    bytes = Text.encodeUtf8 (Builder.build builder)
-    builder = cursorUp newlines0 <> clearFromCursor <> renderTable (estimatesToTable summaries)
-
-newline :: Word8
-newline = 10
+          loop pulls1
+    loop (Driver.pulls (snd . Named.thing <$> summaries0))
 
 -- | Benchmark a function. The result is evaluated to weak head normal form.
 function ::
--- src/ParkBench/Terminal.hs
+++ src/ParkBench/Terminal.hs
@@ -1,7 +1,7 @@
 module ParkBench.Terminal
   ( withTerminal,
-    clearFromCursor,
-    cursorUp,
+    Terminal,
+    renderToTerminal,
     blue,
     green,
     red,
@@ -10,16 +10,37 @@ where
 
 import Control.Exception (bracket_)
 import qualified Data.ByteString as ByteString
+import Data.IORef
+import qualified Data.Text.Encoding as Text (encodeUtf8)
 import ParkBench.Builder (Builder)
 import qualified ParkBench.Builder as Builder
 import ParkBench.Prelude
 import System.IO (hSetEcho, stdin)
 
-withTerminal :: IO a -> IO a
-withTerminal action =
+withTerminal :: (Terminal -> IO a) -> IO a
+withTerminal action = do
+  terminal <- newTerminal
   bracket_ hideCursor showCursor do
     bracket_ (hSetEcho stdin False) (hSetEcho stdin True) do
-      action
+      ByteString.putStr (ByteString.singleton newline)
+      action terminal
+
+newtype Terminal
+  = Terminal (IORef Int)
+
+newTerminal :: IO Terminal
+newTerminal =
+  coerce (newIORef (0 :: Int))
+
+renderToTerminal :: Terminal -> Builder -> IO ()
+renderToTerminal (Terminal lastLineCountRef) bytes0 = do
+  lastLineCount <- readIORef lastLineCountRef
+  let bytes = Text.encodeUtf8 (Builder.build (cursorUp lastLineCount <> clearFromCursor <> bytes0))
+  ByteString.putStr bytes
+  writeIORef lastLineCountRef $! ByteString.count newline bytes
+
+newline :: Word8
+newline = 10
 
 hideCursor :: IO ()
 hideCursor =
